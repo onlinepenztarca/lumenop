467edfecf09a7550d397a12b1a84e423b1c58bdc
Horváth Dániel <daniel93.horvath@gmail.com>
2017-09-13 01:16:57 +0200

    teszt

--- app/BrandCategoryRelationsModel.php
+++ app/BrandCategoryRelationsModel.php
@@ -2,21 +2,22 @@
 
 namespace App;
 use Illuminate\Database\Eloquent\Model;
-
+use App\BrandsModel;
+use App\CategoriesModel;
 class BrandCategoryRelationsModel extends Model
 {
-	protected $table 	= "";//Ez az a tábla, ami a főkategóriákat és a márkákat összekapcsolja
+	protected $table 	= "";
 	
 	const categoryId	= "";
 	const brandId		= "";
 	
 	public function brands() {
 		
-		return $this->belongsTo("App\BrandsModel", 'boltid', 'id');
+		return $this->belongsTo("App\BrandsModel", self::brandId, BrandsModel::id);
 	}
 	
 	public function categories() {
 		
-		return $this->belongsTo("App\CategoriesModel", 'fid', 'id');
+		return $this->belongsTo("App\CategoriesModel", self::categoryId, CategoriesModel::id);
 	}
 }
